a81c6cc866103a9e36a2b4c0cab7f1a55a299b98
noor396 <75422923+noor396@users.noreply.github.com>
2021-05-12 01:25:48 +0300

    commit

--- joud_app/lib/test/views/sign_out_screen.dart
+++ joud_app/lib/test/views/sign_out_screen.dart
@@ -13,45 +13,54 @@ class SignOutScreen extends StatelessWidget {
     double _screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "Sign Out", //
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Color.fromRGBO(230, 238, 156, 1.0),
+      appBar: AppBar(
+        title: Text(
+          "Sign Out", //
+          style: TextStyle(color: Colors.black),
         ),
-        body: Container(
-          width: _screenWidth,
-          height: _screenHeight,
-          child: Column(
-            children: [
-              SizedBox(
-                height: 200,
-              ),
-              RaisedButton(
-                child: Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.only(top: 5, left: 50, right: 50),
-                  decoration:
-                      BoxDecoration(borderRadius: BorderRadius.circular(20)),
-                  child: Text(
-                    'Sign Out',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => Authenticate()));
-                },
-              ),
-            ],
+        backgroundColor: Color.fromRGBO(230, 238, 156, 1.0),
+      ),
+      body:
+          Column(
+        children: [
+          SizedBox(
+            height: 200,
           ),
-        ));
+         
+                     GestureDetector(
+                          onTap: () {
+                            FirebaseAuth.instance.signOut();
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => Authenticate()));
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            height: 45,
+                            width: MediaQuery.of(context)
+                                .size
+                                .width/1.2, 
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(colors: [
+                              //  const Color.fromRGBO(215, 204, 200, 1),
+                                const Color.fromRGBO(166, 155, 151, 0.7),
+                              const Color.fromRGBO(230, 238, 156, 1.0),
+                              ]),
+                              borderRadius: BorderRadius.circular(30),
+                            ),
+                            child: Text(
+                              'Sign Out', //"signup_up"
+                              style: TextStyle(fontSize: 19,
+                              fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+          Container(
+               
+              ),
+        ],
+      ),
+      //)
+    );
   }
 }
