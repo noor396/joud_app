e389ee795923048bf087b57d942784bdbe5719e7
Rama103ra <ramaothman.irshedat@yahoo.com>
2021-05-15 16:29:26 +0300

    Liana Rama edit 1

--- joud_app/lib/test/views/conversation.dart
+++ joud_app/lib/test/views/conversation.dart
@@ -1,85 +1,76 @@
-import 'package:chat_search/helper/constants.dart';
-import 'package:chat_search/services/database.dart';
-import 'package:chat_search/widgets/widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:joud_app/test/helper/constants.dart';
+import 'package:joud_app/test/services/database.dart';
+import 'package:joud_app/widgets/widget.dart';
 
 class ConversationScreen extends StatefulWidget {
-
   final String chatRoomId;
   final String msgReceiver;
 
-
-
   ConversationScreen(this.chatRoomId, this.msgReceiver);
-
-
 
   @override
   _ConversationScreenState createState() => _ConversationScreenState();
 }
 
 class _ConversationScreenState extends State<ConversationScreen> {
-
   //We will use stream for real-time chat
   DatabaseMethods databaseMethods = new DatabaseMethods();
   TextEditingController messageController = new TextEditingController();
   Stream messageStream;
 
-
-  Widget chatMessageList(){
-
-      return Container(
-        //color: Colors.amberAccent,
-        height: 460,
-        child: StreamBuilder(
-          stream: messageStream,
-          builder: (context, snapshot){
-            return snapshot.hasData ?  ListView.builder( // to avoid returning null
-                itemCount: snapshot.data.docs.length,
-                itemBuilder: (context, index){
-                  return MessageTile(
-                    message: snapshot.data.docs[index].data()["message"],
-                    isSenderMe: Constants.myName == snapshot.data.docs[index].data()["sender"],
-                  );
-                }) : Container();
-          },
-        ),
-      );
+  Widget chatMessageList() {
+    return Container(
+      //color: Colors.amberAccent,
+      height: 460,
+      child: StreamBuilder(
+        stream: messageStream,
+        builder: (context, snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  // to avoid returning null
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    return MessageTile(
+                      message: snapshot.data.docs[index].data()["message"],
+                      isSenderMe: Constants.myName ==
+                          snapshot.data.docs[index].data()["sender"],
+                    );
+                  })
+              : Container();
+        },
+      ),
+    );
   }
 
-
-
-
-  sendMessage(){
+  sendMessage() {
     FocusScope.of(context).unfocus();
-    if (messageController.text.isNotEmpty)
-      {
-        Map<String, dynamic> messageMap = {
-          'message' : messageController.text,
-          'sender' : Constants.myName,
-          'time' : DateTime.now(),
-          'receiver': widget.msgReceiver,
-          //here add picture
-        };
-        databaseMethods.addConversationMessages(widget.chatRoomId, messageMap, widget.msgReceiver);
-        messageController.text = "";
-      }
+    if (messageController.text.isNotEmpty) {
+      Map<String, dynamic> messageMap = {
+        'message': messageController.text,
+        'sender': Constants.myName,
+        'time': DateTime.now(),
+        'receiver': widget.msgReceiver,
+        //here add picture
+      };
+      databaseMethods.addConversationMessages(
+          widget.chatRoomId, messageMap, widget.msgReceiver);
+      messageController.text = "";
+    }
     messageController.clear();
   }
 
   @override
   void initState() {
-    databaseMethods.getConversationMessages(widget.chatRoomId).then((val){
+    databaseMethods.getConversationMessages(widget.chatRoomId).then((val) {
       setState(() {
         messageStream = val;
       });
     });
     super.initState();
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -97,8 +88,8 @@ class _ConversationScreenState extends State<ConversationScreen> {
                   IconButton(
                     icon: Icon(Icons.photo),
                     iconSize: 25.0,
-                    color: Color.fromRGBO(166,155,151,1),
-                    onPressed: (){},
+                    color: Color.fromRGBO(166, 155, 151, 1),
+                    onPressed: () {},
                   ),
                   Expanded(
                     child: TextField(
@@ -106,24 +97,25 @@ class _ConversationScreenState extends State<ConversationScreen> {
                       decoration: InputDecoration(
                         border: InputBorder.none,
                         enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Color.fromRGBO(166,155,151,1) ),
+                          borderSide: BorderSide(
+                              color: Color.fromRGBO(166, 155, 151, 1)),
                         ),
                         hintText: 'Send a message...',
-                        hintStyle: TextStyle(
-                            color: Color.fromRGBO(166,155,151,1)),
+                        hintStyle:
+                            TextStyle(color: Color.fromRGBO(166, 155, 151, 1)),
                       ),
                     ),
                   ),
                   GestureDetector(
-                    onTap: (){
+                    onTap: () {
                       sendMessage();
                     },
                     child: IconButton(
-                      color: Color.fromRGBO(189,193,146,1),
-                      disabledColor: Color.fromRGBO(166,155,151,1),
+                      color: Color.fromRGBO(189, 193, 146, 1),
+                      disabledColor: Color.fromRGBO(166, 155, 151, 1),
                       iconSize: 25.0,
                       icon: Icon(Icons.send_rounded),
-                      onPressed:null,
+                      onPressed: null,
                     ),
                   ),
                 ],
@@ -141,15 +133,10 @@ class MessageTile extends StatelessWidget {
   final bool isSenderMe;
   final String msgReceiver;
 
-
-
   MessageTile({this.message, this.isSenderMe, this.msgReceiver});
-
 
   @override
   Widget build(BuildContext context) {
-
-
     return Container(
       padding: EdgeInsets.only(
           top: 10,
@@ -158,44 +145,43 @@ class MessageTile extends StatelessWidget {
           right: isSenderMe ? 15 : 0),
       alignment: isSenderMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Container(
-          margin: isSenderMe
-              ? EdgeInsets.only(left: 30)
-              : EdgeInsets.only(right: 30),
-          padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
-          decoration: BoxDecoration(
-            borderRadius: isSenderMe ? BorderRadius.only(
-                topLeft: Radius.circular(14),
-                topRight: Radius.circular(14),
-                bottomLeft: Radius.circular(23)
-            ) :
-            BorderRadius.only(
-                topLeft: Radius.circular(14),
-                topRight: Radius.circular(14),
-                bottomRight: Radius.circular(23)),
+        margin:
+            isSenderMe ? EdgeInsets.only(left: 30) : EdgeInsets.only(right: 30),
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+        decoration: BoxDecoration(
+            borderRadius: isSenderMe
+                ? BorderRadius.only(
+                    topLeft: Radius.circular(14),
+                    topRight: Radius.circular(14),
+                    bottomLeft: Radius.circular(23))
+                : BorderRadius.only(
+                    topLeft: Radius.circular(14),
+                    topRight: Radius.circular(14),
+                    bottomRight: Radius.circular(23)),
             gradient: LinearGradient(
-                colors: isSenderMe ? [
-                const Color.fromRGBO(240,244,195,1),
-                const Color.fromRGBO(189,193,146,0.8),
-                ]
-                : [
-                const Color.fromRGBO(215,204,200,1),
-                const Color.fromRGBO(166,155,151,0.7),
-            ],
-          )
-      ),
-      child: Column(
-        children: [
-         // Text(isSenderMe ? Constants.myName :''),
-          Text(message,
-              textAlign: TextAlign.start,
-              style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontFamily: 'OverpassRegular',
-                  fontWeight: FontWeight.w400)),
-        ],
-      ),
+              colors: isSenderMe
+                  ? [
+                      const Color.fromRGBO(240, 244, 195, 1),
+                      const Color.fromRGBO(189, 193, 146, 0.8),
+                    ]
+                  : [
+                      const Color.fromRGBO(215, 204, 200, 1),
+                      const Color.fromRGBO(166, 155, 151, 0.7),
+                    ],
+            )),
+        child: Column(
+          children: [
+            // Text(isSenderMe ? Constants.myName :''),
+            Text(message,
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontFamily: 'OverpassRegular',
+                    fontWeight: FontWeight.w400)),
+          ],
         ),
+      ),
     );
   }
 }
--- joud_app/lib/test/views/search.dart
+++ joud_app/lib/test/views/search.dart
@@ -1,8 +1,8 @@
-import 'package:chat_search/helper/constants.dart';
-import 'package:chat_search/services/database.dart';
-import 'package:chat_search/widgets/widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:joud_app/test/helper/constants.dart';
+import 'package:joud_app/test/services/database.dart';
+import 'package:joud_app/widgets/widget.dart';
 import 'conversation.dart';
 
 class Search extends StatefulWidget {
@@ -11,55 +11,64 @@ class Search extends StatefulWidget {
 }
 
 class _SearchState extends State<Search> {
-
-  TextEditingController searchTextEditingController = new TextEditingController();
+  TextEditingController searchTextEditingController =
+      new TextEditingController();
   DatabaseMethods databaseMethods = new DatabaseMethods();
   QuerySnapshot searchSnapshot;
 
   Widget searchList() {
-    return searchSnapshot != null ?  ListView.builder(
-      itemCount: searchSnapshot.docs.length ,
-      shrinkWrap: true,
-      itemBuilder: (context, index){
-        return SearchTile(
-          username: searchSnapshot.docs[index].data()["username"],
-          email: searchSnapshot.docs[index].data()["email"],
-        );
-      }) :   Container();
+    return searchSnapshot != null
+        ? ListView.builder(
+            itemCount: searchSnapshot.docs.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              return SearchTile(
+                username: searchSnapshot.docs[index].data()["username"],
+                email: searchSnapshot.docs[index].data()["email"],
+              );
+            })
+        : Container();
   }
 
-
   initiateSearch() {
-    databaseMethods.getUserByUsername(searchTextEditingController.text.trim()).then((snapshot){
+    databaseMethods
+        .getUserByUsername(searchTextEditingController.text.trim())
+        .then((snapshot) {
       setState(() {
-        searchSnapshot = snapshot;   //update the value, avoids showing the same content again
+        searchSnapshot =
+            snapshot; //update the value, avoids showing the same content again
         print("$searchSnapshot");
       });
     });
   }
 
-  createChatRoomStartConversation({String userName }){
+  createChatRoomStartConversation({String userName}) {
+    if (userName != Constants.myName) {
+      String chatRoomId = getChatRoomId(userName, Constants.myName);
+      List<String> users = [
+        userName,
+        Constants.myName
+      ]; // to store the data locally we used the share preference method
+      Map<String, dynamic> chatRoomMap = {
+        "users": users,
+        "chatRoomId": chatRoomId
+      };
 
-    if (userName != Constants.myName)
-      {
-        String chatRoomId = getChatRoomId(userName, Constants.myName);
-        List <String> users = [userName, Constants.myName]; // to store the data locally we used the share preference method
-        Map<String, dynamic>chatRoomMap = {"users": users, "chatRoomId":chatRoomId };
-
-        DatabaseMethods().createChatRoom(chatRoomId, chatRoomMap);
-        Navigator.push(context, MaterialPageRoute(
-            builder: (context)=> ConversationScreen(chatRoomId, userName)
-        ));
-      }
-    else{
-      final snackBar = SnackBar(content: Text("you can't send a messages to yourself ! "), backgroundColor: Color.fromRGBO(127,0,0,1));
+      DatabaseMethods().createChatRoom(chatRoomId, chatRoomMap);
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => ConversationScreen(chatRoomId, userName)));
+    } else {
+      final snackBar = SnackBar(
+          content: Text("you can't send a messages to yourself ! "),
+          backgroundColor: Color.fromRGBO(127, 0, 0, 1));
       ScaffoldMessenger.of(context).showSnackBar(snackBar);
-      print ('you cannot send messages to yourself');
+      print('you cannot send messages to yourself');
     }
-
   }
 
-  Widget SearchTile({String username, String email} ){
+  Widget SearchTile({String username, String email}) {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
       child: Row(
@@ -67,20 +76,27 @@ class _SearchState extends State<Search> {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(username, style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700),),
-              Text(email, style: TextStyle(color: Colors.black, fontWeight: FontWeight.w400),),
+              Text(
+                username,
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
+              ),
+              Text(
+                email,
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.w400),
+              ),
             ],
           ),
           Spacer(),
           GestureDetector(
-            onTap: (){
+            onTap: () {
               createChatRoomStartConversation(userName: username);
             },
             child: Container(
               decoration: BoxDecoration(
-                  color: Color.fromRGBO(215,204,200,1 ),
-                  borderRadius: BorderRadius.circular(30)
-              ),
+                  color: Color.fromRGBO(215, 204, 200, 1),
+                  borderRadius: BorderRadius.circular(30)),
               padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
               child: Text('Send Message'),
             ),
@@ -90,7 +106,7 @@ class _SearchState extends State<Search> {
     );
   }
 
-  void initState(){
+  void initState() {
     super.initState();
   }
 
@@ -108,25 +124,24 @@ class _SearchState extends State<Search> {
                 height: 50,
                 decoration: new BoxDecoration(
                   borderRadius: new BorderRadius.circular(30.0),
-                  gradient: LinearGradient(
-                      colors: [
-                        //Green Fading
-                        //const Color.fromRGBO( 240,244,195,0.4),
-                        //const Color.fromRGBO( 255,255,246,1),
-                        // const Color.fromRGBO( 189,193,146,1),
+                  gradient: LinearGradient(colors: [
+                    //Green Fading
+                    //const Color.fromRGBO( 240,244,195,0.4),
+                    //const Color.fromRGBO( 255,255,246,1),
+                    // const Color.fromRGBO( 189,193,146,1),
 
-                        //Brownish Fading
-                        const Color.fromRGBO( 215,204,200,1.5),
-                        const Color.fromRGBO( 255,255,251,1),
-                      ]
-                  ),
+                    //Brownish Fading
+                    const Color.fromRGBO(215, 204, 200, 1.5),
+                    const Color.fromRGBO(255, 255, 251, 1),
+                  ]),
                 ),
                 padding: EdgeInsets.symmetric(horizontal: 20),
                 child: Row(
                   children: [
                     Expanded(
                       child: TextField(
-                        controller: searchTextEditingController, // gets the text written in the search bar
+                        controller:
+                            searchTextEditingController, // gets the text written in the search bar
                         decoration: InputDecoration(
                           hintText: 'Search...',
                           hintStyle: TextStyle(color: Colors.black54),
@@ -142,7 +157,7 @@ class _SearchState extends State<Search> {
                         icon: Icon(Icons.search),
                         iconSize: 28.0,
                         color: Colors.black54,
-                        onPressed: null ,
+                        onPressed: null,
                       ),
                     ),
                   ],
@@ -157,8 +172,8 @@ class _SearchState extends State<Search> {
   }
 }
 
-
-getChatRoomId(String a, String b) { // helps us generate a unique chatRoomId
+getChatRoomId(String a, String b) {
+  // helps us generate a unique chatRoomId
   if (a.substring(0, 1).codeUnitAt(0) > b.substring(0, 1).codeUnitAt(0)) {
     return "$b\_$a";
   } else {
